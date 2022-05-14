636db0adae4df3ff04b615031fd6fcd7ab13b94e
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2022-05-14 15:17:24 +0300

    Config colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Circular Medium", "sans-serif"],
+        bold: ["Circular Bold", "sans-serif"],
+        light: ["Circular Light", "sans-serif"],
+      },
+    },
+    colors: {
+      green: "#1db954",
+      black: "#191414",
+      white: "#ffffff",
+    },
   },
   plugins: [],
 };
