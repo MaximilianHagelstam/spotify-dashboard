7ebb360fc830db9d71a427d19331d19180e9846f
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2022-05-19 18:16:39 +0300

    Remove playlist analyser link

--- src/layout/navLinks.ts
+++ src/layout/navLinks.ts
@@ -6,7 +6,6 @@ interface NavLink {
 const navLinks: NavLink[] = [
   { name: "Top Tracks", href: "/top-tracks" },
   { name: "Top Artists", href: "/top-artists" },
-  { name: "Playlist Analyser", href: "/playlist-analyser" },
 ];
 
 export default navLinks;
