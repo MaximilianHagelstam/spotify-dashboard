13c670cb8650c0ddee11986225d533bd02b3b2f4
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2022-05-31 16:41:28 +0300

    Add swr pagination

---
+++ src/hooks/usePagination.ts
@@ -0,0 +1,37 @@
+import useSWRInfinite from "swr/infinite";
+import useAuth from "./useAuth";
+
+const usePagination = <T>(
+  url: string,
+  pageSize: number,
+  timeRange: "short_term" | "medium_term" | "long_term"
+) => {
+  const { fetcher } = useAuth();
+
+  const getKey = (pageIndex: number, previousPageData: T[]) => {
+    if (previousPageData && !previousPageData.length) return null;
+
+    return `${url}?time_range=${timeRange}&limit=${pageSize}&offset=${
+      pageSize * pageIndex
+    }`;
+  };
+
+  const { data, error, size, setSize, isValidating } = useSWRInfinite<T[]>(
+    getKey,
+    fetcher
+  );
+
+  const paginatedData = data?.flat();
+  const isReachedEnd = data && data[data.length - 1].length < pageSize;
+
+  return {
+    data: paginatedData,
+    loading: isValidating,
+    error,
+    size,
+    setSize,
+    isReachedEnd,
+  };
+};
+
+export default usePagination;
--- src/pages/top-tracks.tsx
+++ src/pages/top-tracks.tsx
@@ -1,130 +1,31 @@
 import type { NextPage } from "next";
-import { useEffect, useState } from "react";
 import Card from "../components/Card";
 import ErrorPage from "../components/ErrorPage";
 import Loading from "../components/Loading";
-import useAuth from "../hooks/useAuth";
+import usePagination from "../hooks/usePagination";
 import Track from "../interfaces/Track";
 import Layout from "../layout";
-import { getTopTracks } from "../lib/api";
-import classNames from "../lib/classNames";
 
 const TopTracks: NextPage = () => {
   const LOAD_MORE_LIMIT = 8;
 
-  const { token } = useAuth();
-  const [tracks, setTracks] = useState<Track[]>([]);
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [offset, setOffset] = useState(0);
-  const [timeRange, setTimeRange] = useState<
-    "short_term" | "medium_term" | "long_term"
-  >("short_term");
-  const [hasMore, setHasMore] = useState(true);
-
-  const loadMore = async () => {
-    try {
-      setLoading(true);
-      const newOffset = offset + LOAD_MORE_LIMIT;
-
-      const newTracks = await getTopTracks(
-        token,
-        timeRange,
-        LOAD_MORE_LIMIT,
-        newOffset
-      );
-
-      setOffset(newOffset);
-
-      if (newTracks.length === 0) {
-        setHasMore(false);
-        setLoading(false);
-        return;
-      }
-
-      setTracks([...tracks, ...newTracks]);
-      setLoading(false);
-    } catch (err) {
-      setError(true);
-    }
-  };
-
-  useEffect(() => {
-    const fetchTracks = async () => {
-      try {
-        const topTracks = await getTopTracks(
-          token,
-          timeRange,
-          LOAD_MORE_LIMIT,
-          0
-        );
-
-        setOffset(0);
-        setHasMore(true);
-        setTracks(topTracks);
-        setLoading(false);
-      } catch (err) {
-        setError(true);
-      }
-    };
-
-    fetchTracks();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [timeRange]);
+  const {
+    data: tracks,
+    error,
+    loading,
+    size,
+    setSize,
+    isReachedEnd,
+  } = usePagination<Track>(
+    "https://api.spotify.com/v1/me/top/tracks",
+    LOAD_MORE_LIMIT,
+    "short_term"
+  );
 
   if (error) return <ErrorPage />;
 
   return (
     <Layout>
-      <div>
-        <button
-          disabled={loading}
-          onClick={() => {
-            setTimeRange("short_term");
-          }}
-          className={classNames(
-            timeRange === "short_term" ? "bg-white text-black" : "text-white",
-            "border-white border-2 font-bold py-2 px-6 rounded-full mb-8 hover:bg-white hover:text-black"
-          )}
-        >
-          Short
-        </button>
-        <button
-          disabled={loading}
-          onClick={() => {
-            setTimeRange("medium_term");
-          }}
-          className={classNames(
-            timeRange === "medium_term" ? "bg-white text-black" : "text-white",
-            "border-white border-2 font-bold py-2 px-6 rounded-full mb-8 hover:bg-white hover:text-black ml-4"
-          )}
-        >
-          Medium
-        </button>
-        <button
-          disabled={loading}
-          onClick={() => {
-            setTimeRange("long_term");
-          }}
-          className={classNames(
-            timeRange === "long_term" ? "bg-white text-black" : "text-white",
-            "border-white border-2 font-bold py-2 px-6 rounded-full mb-8 hover:bg-white hover:text-black ml-4"
-          )}
-        >
-          Long
-        </button>
-
-        <span className="text-gray-text text-lg flex justify-center mb-4 md:ml-8 md:mb-0 md:inline">
-          {timeRange === "short_term"
-            ? "Last 4 weeks"
-            : timeRange === "medium_term"
-            ? "Last 6 months"
-            : timeRange === "long_term"
-            ? "Last several years"
-            : ""}
-        </span>
-      </div>
-
       {tracks && (
         <div className="grid grid-cols-2 gap-6 sm:grid-cols-3 lg:grid-cols-4 mb-8">
           {tracks.map((track, idx) => (
@@ -135,10 +36,10 @@ const TopTracks: NextPage = () => {
 
       {loading && <Loading />}
 
-      {hasMore && (
+      {!isReachedEnd && !loading && (
         <div className="grid place-items-center">
           <button
-            onClick={() => loadMore()}
+            onClick={() => setSize(size + 1)}
             disabled={loading}
             className="bg-white text-black font-bold py-2 px-6 rounded-full mb-8 hover:scale-105"
           >
