3f223d666088b1de370a638f9df48b7732dd9897
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2022-06-01 10:32:47 +0300

    Add about page

--- src/layout/navLinks.ts
+++ src/layout/navLinks.ts
@@ -6,6 +6,7 @@ interface NavLink {
 const navLinks: NavLink[] = [
   { name: "Top Tracks", href: "/top-tracks" },
   { name: "Top Artists", href: "/top-artists" },
+  { name: "About", href: "/about" },
 ];
 
 export default navLinks;
---
+++ src/pages/about.tsx
@@ -0,0 +1,45 @@
+import type { NextPage } from "next";
+import Layout from "../layout";
+
+const About: NextPage = () => {
+  return (
+    <Layout>
+      <div className="max-w-lg font-light">
+        <p>
+          Spotify Dashboard provides an overview of your listening habits. Get
+          your favorite tracks and artists, and view a list of your most recent
+          tracks. The project is completely open source and you can view the
+          code on{" "}
+          <a
+            className="font-sans underline"
+            href="https://github.com/MaximilianHagelstam/spotify-dashboard/"
+          >
+            GitHub.
+          </a>
+        </p>
+
+        <p className="mt-4">
+          This dashboard has no analytics and collects no personal data. The
+          token authorized is stored in cookies. We request access to your top
+          artists, top tracks, and recent tracks. You can read more about
+          Spotify&apos;s API{" "}
+          <a
+            className="font-sans underline"
+            href="https://github.com/MaximilianHagelstam/spotify-dashboard/"
+          >
+            here.
+          </a>
+        </p>
+
+        <p className="mt-4 mb-8">
+          Have fun and happy listening{" "}
+          <span role="img" aria-label="peace sign">
+            ✌️
+          </span>
+        </p>
+      </div>
+    </Layout>
+  );
+};
+
+export default About;
